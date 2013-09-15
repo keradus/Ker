f97bbc819602a6ddc5926b9cd639405d5eb2e8f0
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2013-09-15 19:51:55 +0200

    Utils\Debug - debug class skeleton

---
+++ source/Ker/Utils/Debug.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ker\Utils;
+
+/**
+ * Klasa ze statycznymi metodami użytkowymi służącymi celom debugowym.
+ *
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ * @license MIT
+ * @link https://github.com/keradus/Ker
+ * @date 2013-09-15 19:46:30
+ */
+class Debug
+{
+
+}
