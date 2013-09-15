f87ffd1c3818e1e7da5dd50455957691ada1cc14
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2013-09-15 21:32:29 +0200

    DB - add showDebug method

--- source/Ker/DB.php
+++ source/Ker/DB.php
@@ -45,4 +45,31 @@ class DB
         $this->debug[$_type] = (bool) $_on;
     }
 
+    /**
+     * Metoda wyświetlająca dumpa zapytania.
+     * By zapytanie zostało wyświetlone musi być włączony tryb debugowania dla danego typu zapytań.
+     *
+     * @public
+     * @param string $_type rodzaj zapytan, dla ktorego ustalamy debugowanie, musi byc zdefiniowany w $debug
+     * @param string $_sql zapytanie SQL
+     * @param array [opt = NULL] $_value parametry dla zapytania SQL
+     */
+    public function showDebug($_type, $_sql = NULL, $_params = NULL)
+    {
+        if (!isset($this->debug[$_type])) {
+            throw new \InvalidArgumentException();
+        }
+
+        if (!$this->debug[$_type] && !$this->debug["all"]) {
+            return;
+        }
+
+        $sqlFull = (($_sql && $_params)
+            ? str_replace (array_keys($_params), array_map(function ($_) {return "'$_'";}, array_values($_params)), $_sql)
+            : NULL
+        );
+
+        \Ker\Utils\Debug::dmp(array("trace" => false, "memory" => false,), "SQL DEBUG TYPE: " . strtoupper($_type), $_sql, $_params, $sqlFull);
+    }
+
 }
