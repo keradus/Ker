dfbca61751f774df31e007a7a49478e2aa6d8327
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2013-09-15 21:29:31 +0200

    DB - create DB class skeleton

---
+++ source/Ker/DB.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ker;
+
+/**
+ * Klasa służąca do komunikacji z baza danych. Implementuje wzorzec projektowy adapter dla klasy PDO.
+ * Zapewnia uproszczony interfejs dla najczęstszych akcji.
+ *
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ * @license MIT
+ * @link https://github.com/keradus/Ker
+ * @date 2013-09-15 21:26:43
+ */
+class DB
+{
+
+}
