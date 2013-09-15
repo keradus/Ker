303988570897328a63b1da8af1721bb7acddc202
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2013-09-15 20:26:29 +0200

    Ex\NoData - new exception

---
+++ source/Ker/Ex/NoData.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ker\Ex;
+
+/**
+ * Klasa uszczególniająca wyjątek logiczny (LogicException) - rzucany w sytuacji braku oczekiwanych danych.
+ *
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ * @license MIT
+ * @link https://github.com/keradus/Ker
+ * @date 2013-09-15 20:25:13
+ */
+class NoData extends \LogicException
+{
+
+}
