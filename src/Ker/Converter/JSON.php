99eee30f994de700bf6f58b575cea8667b638031
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-05-28 18:26:01 +0200

    Converter\JSON - new converter

---
+++ src/Ker/Converter/JSON.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Ker\Converter;
+
+/**
+ * Statyczna klasa kodująca dane algorytmem JSON.
+ *
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ * @license MIT
+ * @link https://github.com/keradus/Ker
+ * @static
+ */
+class JSON extends \Ker\Converter
+{
+    /**
+     * Kontener na cache kodowanych danych.
+     *
+     * @type array
+     * @static
+     * @protected
+     */
+    protected static $encodeCache = [];
+
+    /**
+     * Kontener na cache dekodowanych danych.
+     *
+     * @type array
+     * @static
+     * @protected
+     */
+    protected static $decodeCache = [];
+
+    /**
+     * Metoda dekodująca.
+     *
+     * @static
+     * @protected
+     * @param string dane zakodowane
+     * @return string dane odkodowane
+     */
+    protected static function decodeRaw($_data)
+    {
+        $result = json_decode($_data, true);
+        $error = json_last_error();
+
+        if ($error !== JSON_ERROR_NONE) {
+            throw new \InvalidArgumentException("JSON decode error: " . $error);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Metoda kodująca.
+     *
+     * @static
+     * @protected
+     * @param string dane do zakodowania
+     * @return string dane zakodowane
+     */
+    protected static function encodeRaw($_data)
+    {
+        $result = json_encode($_data);
+        $error = json_last_error();
+
+        if ($error !== JSON_ERROR_NONE) {
+            throw new \InvalidArgumentException("JSON encode error: " . $error);
+        }
+
+        return $result;
+    }
+}
