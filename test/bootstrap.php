e1814cd8a55e3d518c93f4e8e06f19278f57cef2
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-06-08 19:43:03 +0200

    test\bootstrap.php - add error_reporting & autoloader'a for Ker in test dir

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
-require dirname(__FILE__) . "/../vendor/autoload.php";
+error_reporting(E_ALL);
+
+$autoloader = require dirname(__FILE__) . "/../vendor/autoload.php";
+
+$autoloader->add("Ker", __DIR__);
